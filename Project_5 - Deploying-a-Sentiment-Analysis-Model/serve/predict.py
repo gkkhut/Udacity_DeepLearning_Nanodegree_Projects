6035a6ea4b886094cc22a88eadee9525d626dd89
Gunjan Khut <gunjan.khut92@gmail.com>
2020-06-25 20:03:58 -0400

    update

--- Project_5 - Deploying-a-Sentiment-Analysis-Model/serve/predict.py
+++ Project_5 - Deploying-a-Sentiment-Analysis-Model/serve/predict.py
@@ -86,10 +86,9 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
     with torch.no_grad():
-        out = model.forward(data)
+        output = model.forward(data)
 
-    result = np.rint(out.numpy())
+    result = np.round(output.numpy())
 
     return result
